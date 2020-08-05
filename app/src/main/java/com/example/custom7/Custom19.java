de2b312a2412e293813a7d80c100fc7f9089493a
killing017 <53915137+killing017@users.noreply.github.com>
2020-08-05 18:53:40 +0530

    hiii

---
+++ app/src/main/java/com/example/custom7/Custom19.java
@@ -0,0 +1,14 @@
+package com.example.custom7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Custom19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom19);
+    }
+}
---
+++ app/src/main/java/com/example/custom7/Custom2.java
@@ -0,0 +1,15 @@
+package com.example.custom7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Custom2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom2);
+
+    }
+}
