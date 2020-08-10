e837656b8e1b88e36dea81a941301223831245fe
amanmishra086 <amanm1408@gmail.com>
2020-08-10 12:26:38 +0530

    Signup Page completed

--- app/src/main/java/com/example/custom7/Custom19.java
+++ app/src/main/java/com/example/custom7/Custom19.java
@@ -2,19 +2,166 @@ package com.example.custom7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Custom19 extends AppCompatActivity {
+    EditText name , email , phone, password;
+    String strname, stremail, strphone, strpassword;
+    String finalResult ;
+    String HttpURL = "https://www.cakiweb.com/mechanic/request-api/api.php";
+    Boolean CheckEditText ;
+    ProgressDialog progressDialog;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom19);
+
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        phone=findViewById(R.id.phone);
+        password=findViewById(R.id.password);
+
+
+
     }
 
-    public void onclicksignup(View view) {
-        startActivity(new Intent(Custom19.this,MobileVerification.class));
+
+
+    public void onClickSubmit(View view) {
+        //startActivity(new Intent(Custom19.this,MobileVerification.class));
+
+        // Checking whether EditText is Empty or Not
+        CheckEditTextIsEmptyOrNot();
+
+        if(CheckEditText){
+
+            // If EditText is not empty and CheckEditText = True then this block will execute.
+
+            UserRegisterFunction("registerUser",strname,stremail, strphone, strpassword);
+
+        }
+        else {
+
+            // If EditText is empty then this block will execute .
+            Toast.makeText(Custom19.this, "Please fill all form fields.", Toast.LENGTH_LONG).show();
+
+        }
+
     }
+    public void CheckEditTextIsEmptyOrNot(){
+
+        strname = name.getText().toString();
+        stremail = email.getText().toString();
+        strphone = phone.getText().toString();
+        strpassword = password.getText().toString();
+
+
+        if(TextUtils.isEmpty(strname) || TextUtils.isEmpty(stremail) || TextUtils.isEmpty(strphone) || TextUtils.isEmpty(strpassword))
+        {
+
+            CheckEditText = false;
+
+        }
+        else {
+
+            CheckEditText = true ;
+        }
+
+    }
+
+    public void UserRegisterFunction(final String method,final String name, final String Email, final String phone, final String password){
+
+        class UserRegisterFunctionClass extends AsyncTask<String,Void,String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog = ProgressDialog.show(Custom19.this,"Loading Data",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+
+                super.onPostExecute(httpResponseMsg);
+
+                progressDialog.dismiss();
+                boolean msg=httpResponseMsg.contains("200");
+
+                if(msg){
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(httpResponseMsg);
+                        String messege= jsonObject.getString("otp");
+
+                        Intent intent=new Intent(Custom19.this,MobileVerification.class);
+                        intent.putExtra("phone",strphone);
+                        intent.putExtra("otp",messege);
+                        startActivity(intent);
+
+                        //Toast.makeText(Custom19.this, messege, Toast.LENGTH_SHORT).show();
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(httpResponseMsg);
+                        String messege = jsonObject.getString("msg");
+                        Toast.makeText(Custom19.this, messege, Toast.LENGTH_SHORT).show();
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                hashMap.put("method",params[0]);
+
+                hashMap.put("customer_name",params[1]);
+
+                hashMap.put("customer_email",params[2]);
+
+                hashMap.put("customer_mobile",params[3]);
+
+                hashMap.put("customer_password",params[4]);
+
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+
+                return finalResult;
+            }
+        }
+
+        UserRegisterFunctionClass userRegisterFunctionClass = new UserRegisterFunctionClass();
+
+        userRegisterFunctionClass.execute(method,name,Email,phone,password);
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/custom7/MobileVerification.java
+++ app/src/main/java/com/example/custom7/MobileVerification.java
@@ -2,13 +2,43 @@ package com.example.custom7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MobileVerification extends AppCompatActivity {
 
+    EditText  otp;
+    TextView phone;
+    String otpno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_verification);
+
+        phone=findViewById(R.id.phone);
+        otp=findViewById(R.id.otp);
+
+        Intent intent=getIntent();
+        String phoneno =intent.getExtras().getString("phone");
+         otpno=intent.getExtras().getString("otp");
+
+        phone.setText(phoneno);
+        otp.setText(otpno);
+
+
+    }
+
+    public void onClickverify(View view) {
+
+        String pass=otp.getText().toString();
+        if(pass.equals(otpno)){
+            Toast.makeText(this, "Otp verified !", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Please enter correct otp", Toast.LENGTH_SHORT).show();
+        }
     }
 }
