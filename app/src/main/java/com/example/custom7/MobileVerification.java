e3042cc00c099feeffaddb4c81616c3fc6e54785
killing017 <53915137+killing017@users.noreply.github.com>
2020-08-12 17:36:56 +0530

    Signin updated

--- app/src/main/java/com/example/custom7/MobileVerification.java
+++ app/src/main/java/com/example/custom7/MobileVerification.java
@@ -2,18 +2,33 @@ package com.example.custom7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class MobileVerification extends AppCompatActivity {
 
     EditText  otp;
     TextView phone;
-    String otpno;
+    String strotp,strphone;
+    String finalResult ;
+    String HttpURL = "https://www.cakiweb.com/mechanic/request-api/api.php";
+    Boolean CheckEditText ;
+    ProgressDialog progressDialog;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +38,98 @@ public class MobileVerification extends AppCompatActivity {
         otp=findViewById(R.id.otp);
 
         Intent intent=getIntent();
-        String phoneno =intent.getExtras().getString("phone");
-        otpno=intent.getExtras().getString("otp");
+        String strphone =intent.getExtras().getString("phone");
+        strotp=intent.getExtras().getString("otp");
 
-        phone.setText(phoneno);
-        otp.setText(otpno);
+        phone.setText(strphone);
+        otp.setText(strotp);
 
 
     }
 
     public void onClickverify(View view) {
 
-        String pass=otp.getText().toString();
-        if(pass.equals(otpno)){
-            Toast.makeText(this, "Otp verified !", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Please enter correct otp", Toast.LENGTH_SHORT).show();
+        CheckEditTextIsEmptyOrNot();
+
+        if(CheckEditText){
+
+            // If EditText is not empty and CheckEditText = True then this block will execute.
+
+            UserRegisterFunction("verifyRegistration", strphone, strotp);
+
         }
+        else {
+
+            // If EditText is empty then this block will execute .
+            Toast.makeText(MobileVerification.this, "Please fill all form fields.", Toast.LENGTH_LONG).show();
+
+        }
+
     }
+    public void CheckEditTextIsEmptyOrNot(){
+
+
+
+
+        if(TextUtils.isEmpty(strphone) || TextUtils.isEmpty(strotp) )
+        {
+
+            CheckEditText = false;
+
+        }
+        else {
+
+            CheckEditText = true ;
+        }
+
+    }
+
+    public void UserRegisterFunction(final String method,final String phone, final String otp){
+
+        class UserVerificationFunctionClass extends AsyncTask<String,Void,String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog = ProgressDialog.show(MobileVerification.this,"Loading Data",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+
+                super.onPostExecute(httpResponseMsg);
+
+                progressDialog.dismiss();
+                boolean msg=httpResponseMsg.contains("200");
+
+                if(msg){
+                  Intent intent=new Intent(MobileVerification.this,Signup.class);
+                  startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MobileVerification.this, "Something is wrong, try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                hashMap.put("method",params[0]);
+
+                hashMap.put("customer_mobile",params[1]);
+                hashMap.put("otp",params[2]);
+
+
+                finalResult = httpParse.postRequest(hashMap, HttpURL);
+
+                return finalResult;
+            }
+        }
+
+        UserVerificationFunctionClass userRegisterFunctionClass = new UserVerificationFunctionClass();
+
+        userRegisterFunctionClass.execute(method,phone,otp);
+    }
+
 }
